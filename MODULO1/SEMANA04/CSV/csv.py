7920ff5cb96590b00febcaa431791362b75aa569
HellynCRV <hellynrat@gmail.com>
2026-07-21 07:42:08 -0300

    Lista02

--- MODULO1/SEMANA04/CSV/csv.py
+++ MODULO1/SEMANA04/CSV/csv.py
@@ -1,22 +1,13 @@
 dados = [
     ['Produto', 'Preço', 'Estoque'],
-    ['Notebook', 4500.00, 10],
-    ['Smartphone', 2500.00, 20],
-    ['Tablet', 1500.00, 15],
-    ['Monitor', 800.00, 5]
+    ['notebook', 2500.00, 10],
+    ['mouse', 50.00, 100],
+    ['teclado', 150.00, 50],
+    ['monitor', 800.00, 5]
 ]
-
 import csv
-with open('produtos.csv', 'w') as arquivo_produtos:
-    escritor = csv.writer(arquivo_produtos)
-    escritor.writerows(dados)
-print("PRINT COM CSV READER")
-with open('produtos.csv', 'r') as arquivo_produtos:
-    escritor = csv.reader(arquivo_produtos)
-    for linha in escritor:
-        print(type(linha))
 
-print("\nPRINT COM OPEN READ()")
-with open('produtos.csv', 'r') as arquivo_produtos:
-    escritor = arquivo_produtos.read()
-    print(type(escritor))
+with open ('produtos.csv', mode='r', encoding='utf-8') as arq:
+    conteudo_bruto = arq.read()
+    print(conteudo_bruto)
+    print(type(conteudo_bruto))
